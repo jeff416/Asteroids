9e931c75f536cc2cb83d55e40ee82eed9a5c742d
Jeff M <17222948+jeff416@users.noreply.github.com>
2025-07-23 22:09:23 -0600

    player character onscreen

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    
     clock = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -14,7 +21,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill(pygame.Color(0,0,0))
+        screen.fill(pygame.Color("black"))
+        player.draw(screen)
         clock.tick(60)
         dt = clock.get_time()
         pygame.display.flip()
